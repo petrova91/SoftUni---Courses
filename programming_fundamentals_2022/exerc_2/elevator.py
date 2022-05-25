aec0cc7a1e3e57be8b4a86efb944b66965a6c37f
petrova91 <101433098+petrova91@users.noreply.github.com>
2022-05-25 16:11:19 +0300

    updated exerc_2

---
+++ programming_fundamentals_2022/exerc_2/elevator.py
@@ -0,0 +1,4 @@
+number_people = int(input())
+capacity_elevator = int(input())
+courses = 0
+people = 0
---
+++ programming_fundamentals_2022/exerc_2/integer_operations.py
@@ -0,0 +1,8 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+result = first_number + second_number
+result = int(result / third_number)
+result *= fourth_number
+print(result)
