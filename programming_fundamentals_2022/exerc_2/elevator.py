5ebca37f76fd80947a7e0aebf33e0838e9cfb99e
petrova91 <101433098+petrova91@users.noreply.github.com>
2022-05-25 16:16:39 +0300

    Changed task 'elevator'

--- programming_fundamentals_2022/exerc_2/elevator.py
+++ programming_fundamentals_2022/exerc_2/elevator.py
@@ -1,4 +1,14 @@
+import math
+
 number_people = int(input())
 capacity_elevator = int(input())
 courses = 0
-people = 0
+full_elevator = math.floor(number_people / capacity_elevator)
+courses += full_elevator
+number_people -= courses * capacity_elevator
+if 0 < number_people <= capacity_elevator:
+    courses += 1
+print(courses)
+
+
+
